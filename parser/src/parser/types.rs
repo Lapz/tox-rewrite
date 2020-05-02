d72c08031ac3fd8420713d2edcba15e7aa3c051f
Lenard Pratt <striderman34@gmail.com>
2020-05-02 23:17:42 +0100

    Remove eat trivias

--- parser/src/parser/types.rs
+++ parser/src/parser/types.rs
@@ -4,7 +4,6 @@ use syntax::T;
 
 impl<'a> Parser<'a> {
     pub(crate) fn parse_type(&mut self) {
-        self.eat_trivias();
         match self.current() {
             IDENT | T![self] => self.parse_ident_type(),
             T!["["] => self.parse_array_type(),
